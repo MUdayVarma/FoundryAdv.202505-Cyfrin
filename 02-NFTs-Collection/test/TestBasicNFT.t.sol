7d2e1e62739decbb336e8295517d7e726f89ba2a
Uday Varma <mudayvarma@gmail.com>
2025-06-22 23:52:22 +0530

    NFT deployment in Sepolia

--- 02-NFTs-Collection/test/TestBasicNFT.t.sol
+++ 02-NFTs-Collection/test/TestBasicNFT.t.sol
@@ -10,8 +10,9 @@ contract TestBasicNFT is Test {
 
     BasicNFT public basicNFT;
     DeployBasicNFT public deployer;
-
-    address 
+    address public USER = makeAddr("user");
+    string public constant PUG = "ifps://QmUPjADFGEKmfohdTaNcWhp7VGk26h5jXDA7v3VtTnTLcW";
+    //"https://bafybeicz57su3zyafexgchnfqej7zuxfa3e4xgz22yzmge6nzkhfmskqs4.ipfs.dweb.link?filename=st-bernard.png";
 
     function setUp() public {
         deployer = new DeployBasicNFT();
@@ -22,6 +23,17 @@ contract TestBasicNFT is Test {
         string memory expectedName = "Dogie";
         string memory actualName = basicNFT.name();
         assertEq(actualName, expectedName);
+
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNFT.mintNFT(PUG);
+
+        assert(basicNFT.balanceOf(USER) == 1);
+        assertEq(basicNFT.tokenURI(0), PUG, "Token URI does not match expected value");
+        assert(keccak256(abi.encodePacked(basicNFT.tokenURI(0))) == keccak256(abi.encodePacked(PUG)));//, "Token URI does not match expected value");
+
 
     }
 
